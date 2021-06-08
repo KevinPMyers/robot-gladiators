38599a29fa00e0332357052ca2318a78978c9324
KevinPMyers <kmyers25@gmail.com>
2021-06-08 16:58:09 -0400

    player blank name bug resolved

--- assets/js/game.js
+++ assets/js/game.js
@@ -83,7 +83,7 @@ var startGame = function () {
 
             // let player know what round they are in, remeber that arrays start at 0 so it needs to have 1 added to it
             window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
-
+           
             // pick new enemy to fight based on the index of the enemy.names array
             var pickedEnemyObj = enemyInfo[i];
 
@@ -168,8 +168,19 @@ var shop = function () {
     }
 };
 
+// get player name function
+var getPlayerName = function() {
+    var name = "";
+
+    while (name === "") || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    console.log("Your robot's name is " + name);
+    return name;
+}
+
 var playerInfo = {
-    name: window.prompt("What is your Robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     // money variable
@@ -224,6 +235,6 @@ console.log(enemyInfo[0].name);
 console.log(enemyInfo[0]['attack']);
 // start the game when the page loads
 
-// startGame();
+startGame();
 
 
